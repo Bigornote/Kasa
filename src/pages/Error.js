4a3fd6050b0555c45593298d515327900aab2c0a
Antoine Buard <a.buard@laposte.net>
2022-10-27 13:37:00 +0200

    Page 404

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
-  return <div></div>;
+  return (
+    <main className="error">
+      <h1>404</h1>
+      <p>"Oups ! La page que vous avez demandé n'existe pas."</p>
+      <div className="link">
+        <Link to="/">Retourner sur la page d'accueil</Link>
+      </div>
+    </main>
+  );
 };
 
 export default Error;
